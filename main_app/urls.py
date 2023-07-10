239d0a40121aab347d20d72a8f83c72dade9248f
Shak94 <shakoi.1994@gmail.com>
2023-07-10 12:08:45 -0400

    Saved progress

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
 
     path('', views.Home.as_view(), name='home'),
-   
+    path('sharks/', views.SharkList.as_view(), name="shark_list"),
+    path('sharks/about/', views.About.as_view(), name='about'),
+
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView
+from typing import Any, Dict
+
 
 # Create your views here.
 
@@ -9,3 +11,13 @@ from django.views.generic.base import TemplateView
 class Home (TemplateView):
     template_name="home.html"
 # This is similar to response.send from express.
+class About(TemplateView):
+    template_name="about.html"
+
+
+class SharkList(TemplateView):
+    template_name="shark_list.html"
+    def get_context_data(self, **kwargs: Any):
+        context= super().get_context_data( **kwargs)
+        context['sharks'] = Shark.objects.all()
+        return context
