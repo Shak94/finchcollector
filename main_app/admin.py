305488c606a5d31170fd7cbc303e2565f55487a3
Shak94 <shakoi.1994@gmail.com>
2023-07-10 23:40:27 -0400

    Finished shark collector

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.contrib import admin
-from .models import Shark 
+from .models import Shark ,SharkInfo
 # Register your models here.
 
-admin.site.register(Shark) # this line will add the model to the admin panel
+admin.site.register(Shark) 
+admin.site.register(SharkInfo)
+# this line will add the model to the admin panel
 # Register your models here.
--- main_app/models.py
+++ main_app/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 
-# Create your models here.
 class Shark(models.Model):
-
     name = models.CharField(max_length=100)
     img = models.CharField(max_length=250)
     bio = models.TextField(max_length=500)
@@ -13,3 +11,12 @@ class Shark(models.Model):
 
     class Meta:
         ordering = ['name']
+
+
+class SharkInfo(models.Model):
+    species = models.CharField(max_length=100)
+    discovered = models.IntegerField(default=0000)
+    shark = models.ForeignKey(Shark, on_delete=models.CASCADE, related_name='more')
+    
+    class Meta:
+        ordering = ['species']
